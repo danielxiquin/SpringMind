3a3e8c4d7e45bb1c586bb162727cb005e24136ac
danielxiquin <danielxiquin802@gmail.com>
2025-04-23 11:47:43 -0600

    se cambio el icon como el tiulo de la web y se elimino un import sin usar de app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import Hero from './components/hero'
 import Menu from './components/menu'
 import Parmeters from './components/parameters'
-import ResultsDisplay from './components/results'
 import './App.css'
 
 function App() {
