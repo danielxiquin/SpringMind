2726edf616cec6e7a63a0d7bec0462873ede644a
danielxiquin <danielxiquin802@gmail.com>
2025-08-12 20:28:19 -0600

    tercera prueba de imagen

--- api/og.js
+++ api/og.js
@@ -1,17 +1,19 @@
 // api/og.js
 export const config = { runtime: 'edge' };
 
-export default function handler() {
-  return new Response(
-    `
-    <svg width="1200" height="630" xmlns="http://www.w3.org/2000/svg">
-      <rect width="100%" height="100%" fill="white"/>
-      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"
-            font-size="80" font-family="Arial">Mi Web v1.2</text>
-    </svg>
-    `,
-    {
-      headers: { 'Content-Type': 'image/svg+xml' },
-    }
-  );
+export default async function () {
+  const target = 'https://springmind.vercel.app/';
+  const imgUrl = `https://i.microlink.io/?url=${encodeURIComponent(
+    target
+  )}&screenshot=true&embed=screenshot.url`;
+
+  const res = await fetch(imgUrl);
+  const buffer = await res.arrayBuffer();
+
+  return new Response(buffer, {
+    headers: {
+      'Content-Type': 'image/png',
+      'Cache-Control': 'public, max-age=3600',
+    },
+  });
 }
