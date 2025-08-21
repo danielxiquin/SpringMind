696c1c38dbbb950d7b66537ba1a4c3d800dd441a
danielxiquin <danielxiquin802@gmail.com>
2025-08-21 16:49:54 -0600

    se agrego estadisticas

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,19 @@ import FinalFooter from './components/final'
 import './App.css'
 
 function App() {
+
+  useEffect(() => {
+    const projectId = 'f47ac10b-58cc-4372-a567-0e02b2c3d479'; // ← UUID real del proyecto
+    const script = document.createElement('script');
+    script.src = 'http://localhost:4321/api/projects/track-secure';
+    script.dataset.project = projectId;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
   const [count, setCount] = useState(0)
   const [resultData, setResultData] = useState(null);
   
